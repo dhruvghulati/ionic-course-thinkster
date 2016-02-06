b1f7065c5e2607093fb4278c5114860fae904839
dhruvghulati <ghulatid@gmail.com>
2016-02-06 18:37:52 +0000

    Committed files just before being able to delete favorites

---
+++ code/www/js/services.js
@@ -0,0 +1,22 @@
+angular.module('songhop.services',[])
+
+.factory('User', function () {
+    var o = {
+        favorites: []
+    }
+
+    o.addSongToFavorites = function (song) {
+        if (!song) return false;
+                  
+        o.favorites.unshift(song);
+    }
+    
+    o.removeSongFromFavorites = function (song){
+        if(!song) return false;
+        
+        o.favorites.splice(index, 1);
+    }
+
+    return o;
+
+});
